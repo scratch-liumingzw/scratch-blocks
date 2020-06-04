8b6fca72df18d97554daea93c1c9abcda09398b3
liumingzw <374406742@qq.com>
2020-06-04 17:30:52 +0800

    只打包项目需要的，可以提高打包速度

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,7 @@ var path = require('path');
 var UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 
 module.exports = [{
-  mode: process.env.NODE_ENV === 'production' ? 'production' : 'development',
+  mode: 'production',
   entry: {
     horizontal: './shim/horizontal.js',
     vertical: './shim/vertical.js'
@@ -20,76 +20,9 @@ module.exports = [{
     filename: '[name].js'
   },
   optimization: {
-    minimize: false
+    minimize: true
   },
   performance: {
     hints: false
   }
-}, {
-  mode: process.env.NODE_ENV === 'production' ? 'production' : 'development',
-  entry: {
-    horizontal: './shim/horizontal.js',
-    vertical: './shim/vertical.js'
-  },
-  output: {
-    library: 'Blockly',
-    libraryTarget: 'umd',
-    path: path.resolve(__dirname, 'dist', 'web'),
-    filename: '[name].js'
-  },
-  optimization: {
-    minimizer: [
-      new UglifyJsPlugin({
-        uglifyOptions: {
-          mangle: false
-        }
-      })
-    ]
-  },
-  plugins: []
-},
-{
-  mode: process.env.NODE_ENV === 'production' ? 'production' : 'development',
-  entry: './shim/gh-pages.js',
-  output: {
-    filename: '[name].js',
-    path: path.resolve(__dirname, 'gh-pages')
-  },
-  optimization: {
-    minimize: false
-  },
-  performance: {
-    hints: false
-  },
-  plugins: [
-      new CopyWebpackPlugin([{
-        from: 'node_modules/google-closure-library',
-        to: 'closure-library'
-      }, {
-        from: 'blocks_common',
-        to: 'playgrounds/blocks_common',
-      }, {
-        from: 'blocks_horizontal',
-        to: 'playgrounds/blocks_horizontal',
-      }, {
-        from: 'blocks_vertical',
-        to: 'playgrounds/blocks_vertical',
-      }, {
-        from: 'core',
-        to: 'playgrounds/core'
-      }, {
-        from: 'media',
-        to: 'playgrounds/media'
-      }, {
-        from: 'msg',
-        to: 'playgrounds/msg'
-      }, {
-        from: 'tests',
-        to: 'playgrounds/tests'
-      }, {
-        from: '*.js',
-        ignore: 'webpack.config.js',
-        to: 'playgrounds'
-      }])
-  ]
 }];
